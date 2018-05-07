745a9a2310f342fc07c4da9d7d1fb5e3f59ad81d
Larry Tenny <ltenny@gmail.com>
2018-05-07 13:17:29 -0500

    Added distorted_inputs method

--- cifar10_input.py
+++ cifar10_input.py
@@ -51,3 +51,58 @@ def read_cifar10(filename_queue):
     return result
     
 
+def _generate_image_and_label_batch(image, label, min_queue_examples, batch_size, shuffle):
+    num_preprocess_threads = 16
+    if shuffle:
+        images, label_batch = tf.train.shuffle_batch(
+            [image, label],
+            batch_size = batch_size,
+            num_threads = num_preprocess_threads,
+            capacity = min_queue_examples)
+    else:
+        images, label_batch = tf.train.batch(
+            [image, label],
+            batch_size = batch_size,
+            num_threads = num_preprocess_threads,
+            capacity = min_queue_examples + 3 * batch_size)
+    
+    tf.summary.image('images', images)
+    return images, tf.reshape(label_batch, [batch_size])
+
+def distorted_inputs(data_dir, batch_size):
+    filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i) for i in xrange(1,6)]
+
+    # make sure each file exists
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('failed to find file: ' + f)
+        
+    # create a queue of the filenames
+    filename_queue = tf.train.string_input_producer(filenames)
+
+    with tf.name_scope('data_augmentation'):
+        read_input = read_cifar10(filename_queue)
+        reshape_image = tf.cast(read_input.uint8image, tf.float32)
+        height = IMAGE_SIZE
+        width = IMAGE_SIZE
+
+        # apply a few distortions
+        distorted_image = tf.random_crop(reshaped_image, [height, width, 3])
+        distorted_image = tf.image.random_flip_left_right(distorted_image)
+        distorted_image = tf.image.random_brightness(distorted_image, max_delta=63)
+        distorted_image = tf.image.random_contrast(distorted_image, lower=0.2,upper=1.8)
+
+        # standardize by centering around the mean and divide by stddev
+        float_image = tf.image.per_image_standardization(distorted_image)
+        
+        float_image.set_shape([height,width])
+        read_input.label.set_shape([1])
+
+        min_fraction_of_examples_in_queue = 0.4
+        min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * min_fraction_of_examples_in_queue)
+        print('Filling queue with %d CIFAR images before starting to train. ' % min_queue_examples)
+
+        return _generate_image_and_label_batch(float_image, read_input.label, min_queue_examples, batch_size, shuffle=True)
+
+
+        
