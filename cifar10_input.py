6078e8a0b986945b642295b5c60152748e70b6b8
Larry Tenny <ltenny@gmail.com>
2018-05-07 12:17:27 -0500

    Initial commit

---
+++ cifar10_input.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from six.moves import xrange
+import tensorflow as tf 
+
+# although the CIFAR-10 are 32x32 we take just the center 24x24
+IMAGE_SIZE = 24
+
+NUM_CLASSES = 10
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+
+# reads a single example from CIFAR-10 files
+def read_cifar10(filename_queue):
+    class CIFAR10Record(object):
+        pass
+    result = CIFAR10Record()
+    
